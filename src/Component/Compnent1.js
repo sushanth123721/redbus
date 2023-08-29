85303f6dae7f25c55bf2dce210d00c597dc47d96
sushanth123721 <sushanth.gunda@gmail.com>
2023-08-29 17:50:13 +0530

    redbusInfo

---
+++ src/Component/Compnent1.js
@@ -0,0 +1,106 @@
+import React from 'react'
+
+const productInfo = [{
+    x : "Booking Cab Online with rYde rYde" ,
+    y : <p>rYde has recognised the public's passion for travel and has made booking a cab online simple. We have about 10,000 cars     covering nearly 60,000 routes across India, all of which are fitted with a 24-hour tracking device and are available for booking online.
+
+    Cabs can be rented, hired, or booked online using our official website or by downloading the redBus app on your mobile phone.
+    
+    All you need to do is provide your reason for hiring, source, and destination. Following that, you must choose the type of vehicle you desire as well as the number of travelers. There is no booking fee at the time of payment.</p>
+},
+{
+    x : "Benefits of rYde Cabs",
+    y : <p>rYde is India’s best and simple online cab booking service. People prefer to book cabs on rYde due to the following benefits
+         Booking a cab on rYde is relatively cost-effective.
+         All the cabs we onboard are verified. rYde checks the quality of each vehicle onboard before sending them to customers.
+         You can pay only 25% to make a booking and pay the balance at the dropping point..
+         Customer support is provided to make a booking as well as during your journey. Our support centers are open 24/7 and 365 days a year. 24/7 Live Chat available.
+         rYde works closely with the best cab service providers in the country. All you need to do is provide necessary details about your journey to start the trip.
+         Once you fill up the form, you get detailed quotations from some of the best cab/taxi operators of your city of travel.</p>
+},
+
+{
+    x : "rYde Promises",
+    y : <p>rYde offers clean and audited cabs to the travelers
+            rYde cabs provide monitored on-time pick-ups
+            rYde provides drivers who are well-trained and highly certified for your safety.</p>
+},
+{
+    x : "Hire us whenever",
+    y : <p>The buses can be hired not only for tourism, but also for leisure activities like picnics, field trips, excursions, and formal requirements like corporate events, meetings, weddings, and more.
+
+    No matter whether you want to hire/rent a vehicle for a whole day or just a few hours, we will meet your needs. Bus for hire is now one click away. We also ensure extra comfort, luxurious and hassle-free experience, and of course, a punctual and expert 
+    service</p>
+},
+{
+    x : "Hiring/Renting A Bus with rYde",
+    y : <p>rYde understands that comfort and enjoyment are important while travelling. Therefore, we have collaborated with some of the finest travel agencies in India to render a perfect trip to our customers. The passengers can opt for air conditioned or non-air conditioned buses. The buses also have push back and recliner seats making it possible for the elderly to travel. Buses are also equipped with LCD TV and DVD player for your entertainment. Buses may also come equipped with mini-refrigerators and ice boxes for refreshment. Facilities like comfy blankets, and charging ports are also available in buses.
+
+    You can also book a sleeper coach bus with rYde. For long-distance night travel, travelling in a sleeper bus is the best option. Sleeper bus booking is possible online with rYde.</p>
+},
+{
+    x : "Holiday with rYde",
+    y : <p>Vacations are best enjoyed in groups. If you are going on a trip with your group, hiring/renting a bus is a good option. You can hire a bus from rYde for your group holiday. rYde offers trustworthy travel services of classic experience and quality. rYde considers the emotions and needs of its passengers and delivers customer-oriented services. You can hire/rent a bus from rYde via our website or app.
+
+    rYde has a variety of buses with seating capacities ranging from 30 to 50, and tempo travellers with seating capacities ranging from 9 to 50. Booking of buses and tempo travellers is moderately priced, keeping the needs of the users in mind.
+    </p>
+},
+{
+    x : "Benefits of rYde",
+    y : <p>Here is what you get when you rent a bus with rYde.
+
+    With the roomy interiors in our buses, you get enough space to relax so that you do not get feel suffocated. It is relatively cost-effective to rent a bus from rYde All the buses we onboard are verified. rYde checks the quality of each vehicle onboard before sending them to customers. You can pay only 25% to make a booking and pay the balance at the time of boarding the vehicle Vehicle can be tracked via GPS. Drivers have an app which allows high quality tracking. Customer support is provided to make a booking as well as during your journey. Our support centers are open 24/7 and 365 days a year.
+    
+    rYde works closely with the best bus service providers in the country. All you need to do is to answer a few simple questions about your trip. Once you fill up the form, you get detailed quotations from some of the best bus operators of your city of travel.</p>
+},
+{
+    x : "Booking Buses Online with rYde",
+    y : <p>rYde has identified the public’s love of travel and has made the renting/hiring of buses and tempo travellers easy. We hold almost 10,000 buses, covering almost 60,000 routes throughout India equipped with a 24 hour tracking device, all available for booking online.
+
+    You can rent, hire, or book buses online though our official website or by downloading our app on your mobile phones.
+    
+    All you have to do is enter your reason for hiring, your starting point, and your place of destination. Following this, you are required to enter the type of vehicle you prefer with the number of travellers. No booking cost is levied at the time of payment.</p>
+},
+{
+    x : "Hire/Rent a bus online only with rYde",
+    y : <p>India has some of the exemplary architectural wonders such as Qutb Minar, India Gate, Red Fort, and Lotus Temple in Delhi and Charminar, and Hussain Sagar in Hyderabad. There is so much to explore in Incredible India. Visit the amazing wonders in the country while holidaying in any of the touristic cities by hiring/renting a bus online with rYde.
+
+    Touring in a country where cities have confusing streets and growing traffic can be difficult. Hiring/renting a vehicle with rYde ensures that you and your group have a comfortable travel.</p>
+},
+{
+    x: "Things to know about rYde",
+    y : <p>rYde is amongst the list of renowned online bus booking websites. It has earned its place among the best due to its ability to cater to every customer’s specific travel needs while also providing them with nothing but the best travel experience. In doing so, they provide their customers with a plethora of bus booking options. It works with only the best operators and ensures that only the most experienced drivers drive buses. rYde makes booking an ordinary vehicle, such as a tempo traveller or minibus, for a group vacation much more comfortable and more accessible. rYde offers customers with air-conditioned as well as non-air-conditioned buses to choose from and book. It also provides sleeper buses that operate at night. rYde and rYde bus rental can be done either through the use of the official website or the easy to download rYde application.
+
+    rYde has been making sure to take care of all its customer’s health and also play a role in curbing the further spread of COVID-19. In a bid to do just this, rYde has come out with an initiative called Safety+ under which there are specific preventive guidelines all buses must adhere to. Some of these are as follows:
+    Passengers, as well as staff and employees, are strictly required to wear masks at all times
+    Suspension of the complimentary provision of cover-ups temporarily to prevent contact spread
+    Mandatory requirement of sanitisers and disinfectants
+    Deep and thorough sanitation of all vehicles</p>
+},
+{
+    x : "While booking a bus online, one must keep in mind specific tips. These are listed as follows:",
+    y : <p>It is essential to make sure that the online bus booking service that you choose is reliable and trustworthy like rYde.
+    Look to see if the online bus booking service of your choice meets all your expectations and services all your specific travel needs. rYde, through the provision of the filters option, allows its passengers to curate a trip by following their travel needs.
+    Make sure to carry a sanitiser and mask to all the locations you are visiting.</p>
+}]
+
+function Compnent1() {
+  return (
+    <div>
+        {
+            productInfo.map((a) => {
+                return (
+                    <div>
+                        <h3>{a.x}</h3>
+                        <p>{a.y}</p>
+                    </div>
+
+                )
+            })
+        }
+    </div>
+
+  )
+}
+
+export default Compnent1
